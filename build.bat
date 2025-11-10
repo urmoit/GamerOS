adb8bf7c048e0c52991334fba09e53142f446c51
ChosenTechies <89414990+urmoit@users.noreply.github.com>
2025-11-10 02:58:53 +0200

    Delete build.bat

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-REM Build GamerOS
-
-REM Build the Docker image
-docker build -t gameros-buildenv buildenv
-
-REM Run the build
-docker run --rm -v "%cd%:/root/env" gameros-buildenv bash -c "cd /root/env && make build-x86_64"
